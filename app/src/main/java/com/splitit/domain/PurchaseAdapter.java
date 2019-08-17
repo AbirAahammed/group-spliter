e756a9a58b15aa71af9bf38c6604649e8a451a4c
abir <sakiba@myumanitoba.ca>
2019-08-17 03:26:17 -0500

    Back to old state.

---
+++ app/src/main/java/com/splitit/domain/PurchaseAdapter.java
@@ -0,0 +1,36 @@
+package com.splitit.domain;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class PurchaseAdapter extends BaseAdapter {
+
+    Activity context;
+    ArrayList <Purchase> purchases;
+    private static LayoutInflater inflater = null;
+
+
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
--- app/src/main/java/com/splitit/presentation/MainActivity.java
+++ app/src/main/java/com/splitit/presentation/MainActivity.java
@@ -16,6 +16,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView recyclerView = findViewById(R.id.recyclerMenu);
-        recyclerView.l
     }
 }
