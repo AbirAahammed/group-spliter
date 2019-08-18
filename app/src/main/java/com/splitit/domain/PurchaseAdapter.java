b101902c82013ef437f99e096436eddcc6f7eedb
abirAsakib <ahammed.abir47@gmail.com>
2019-08-17 22:48:49 -0500

    nothing important

--- app/src/main/java/com/splitit/domain/PurchaseAdapter.java
+++ app/src/main/java/com/splitit/domain/PurchaseAdapter.java
@@ -1,10 +1,14 @@
 package com.splitit.domain;
 
 import android.app.Activity;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.splitit.R;
 
 import java.util.ArrayList;
 
@@ -14,23 +18,36 @@ public class PurchaseAdapter extends BaseAdapter {
     ArrayList <Purchase> purchases;
     private static LayoutInflater inflater = null;
 
+    public PurchaseAdapter(Activity context, ArrayList<Purchase> purchases){
+        this.context = context;
+        this.purchases = purchases;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
 
     public int getCount() {
-        return 0;
+        return purchases.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return purchases.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        View itemView = view;
+        itemView = (itemView == null) ? inflater.inflate(R.layout.list_item, null): itemView;
+        TextView name = itemView.findViewById(R.id.Name);
+        TextView price = itemView.findViewById(R.id.Price);
+        Purchase purchase = purchases.get(i);
+        name.setText(purchase.getPurchaseName());
+        name.setText(purchase.getPrice());
+        return itemView;
     }
 }
--- app/src/main/java/com/splitit/presentation/MainActivity.java
+++ app/src/main/java/com/splitit/presentation/MainActivity.java
@@ -5,16 +5,46 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.splitit.R;
+import com.splitit.domain.Purchase;
+import com.splitit.domain.PurchaseAdapter;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private ArrayList<Purchase> purchases = new ArrayList<>();
+    private PurchaseAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView recyclerView = findViewById(R.id.recyclerMenu);
+        populateListView();
+    }
+
+    public void populateListView() {
+        listView =  findViewById(R.id.listViewPurchases);
+        purchases.add(new Purchase(1,"Veggi", 34, new Date()));
+        purchases.add(new Purchase(2,"meat", 34, new Date()));
+        purchases.add(new Purchase(3,"rice", 34, new Date()));
+        purchases.add(new Purchase(4,"chicken", 34, new Date()));
+        purchases.add(new Purchase(5,"duck", 34, new Date()));
+        purchases.add(new Purchase(6,"eto", 34, new Date()));
+        purchases.add(new Purchase(7,"beef", 34, new Date()));
+        adapter = new PurchaseAdapter(this, purchases);
+        listView.setAdapter(adapter);
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Toast.makeText(MainActivity.this, "Click to item: " + i, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
     }
 }
